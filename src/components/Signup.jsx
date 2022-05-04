adff6dff08b57c25efab3a613c60c33fc97ed657
Nivaldo Farias <badivia.f@gmail.com>
2022-05-04 11:24:47 -0300

    Added Signup component, refactor data context

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,11 +1,118 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 
 import logo from "./../assets/mywallet-logo.png";
 
 function SignUp() {
-  return <></>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    image: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  function buildSignupPage() {
+    const validateForm =
+      formData.name?.length > 0 &&
+      formData.email?.length > 0 &&
+      formData.password?.length > 0 &&
+      formData.image?.length > 0
+        ? ""
+        : "disabled";
+
+    return (
+      <>
+        <figure>
+          <img src={logo} alt="logo" />
+          <figcaption>MyWallet</figcaption>
+        </figure>
+        <form className="form-group" onSubmit={handleSignUp}>
+          <div className="input-group">
+            <input
+              type="text"
+              value={formData.name}
+              name="name"
+              onChange={handleInputChange}
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Nome</label>
+          </div>
+          <div className="input-group">
+            <input
+              type="text"
+              value={formData.email}
+              name="email"
+              onChange={handleInputChange}
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>E-mail</label>
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              value={formData.password}
+              name="password"
+              onChange={handleInputChange}
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Senha</label>
+          </div>
+          <div className="input-group">
+            <input
+              type="text"
+              value={formData.image}
+              name="image"
+              onChange={handleInputChange}
+              required
+            />
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label>Avatar URL</label>
+          </div>
+          <button className={validateForm} type="submit">
+            Cadastrar
+          </button>
+          <Link to="/">Já possui uma conta? Faça Login</Link>
+        </form>
+      </>
+    );
+
+    function handleSignUp(e) {
+      e.preventDefault();
+
+      const promise = axios.post("localhost:5000/mywallet/auth/signup", {
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+      });
+
+      promise.then((response) => {
+        console.log(response);
+        navigate("/");
+      });
+      promise.catch((error) => console.log(error.response));
+    }
+
+    function handleInputChange(e) {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+  }
+
+  const singupPage = buildSignupPage();
+
+  return (
+    <>
+      <main id="signup-page">{singupPage}</main>
+    </>
+  );
 }
 
 export default SignUp;
--- src/components/index.js
+++ src/components/index.js
@@ -2,26 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useLocalStorage } from "./../hooks/useLocalStorage";
-import UserContext from "./../hooks/UserContext";
-import TokenContext from "./../hooks/TokenContext";
+import DataContext from "./../hooks/DataContext";
 
 import Login from "./Login";
 import SignUp from "./Signup";
 
 export default function App() {
-  const [user, setUser] = useLocalStorage("user", null);
-  const [token, setToken] = useLocalStorage("token", null);
+  const [data, setData] = useLocalStorage("data", null);
 
   return (
-    <TokenContext.Provider value={{ token, setToken }}>
-      <UserContext.Provider value={{ user, setUser }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-          </Routes>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </TokenContext.Provider>
+    <DataContext.Provider value={{ data, setData }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </DataContext.Provider>
   );
 }
